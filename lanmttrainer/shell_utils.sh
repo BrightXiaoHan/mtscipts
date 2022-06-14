7dbbdf87ae9eae52f901e327c688f17cfe8330e5
BrightXiaoHan <beatmight@gmail.com>
2022-06-14 16:10:31 +0800

    update code

--- lanmttrainer/shell_utils.sh
+++ lanmttrainer/shell_utils.sh
@@ -8,9 +8,25 @@ if [ -z "${DATA_DIR}" ]; then
   exit 1
 fi
 
+if [ -z "${TRAIN_DIR}" ]; then
+  echo "Please set TRAIN_DIR in the environment variables."
+fi
+
+# create TRAIN_DIR if it doesn't exist
+if [ ! -d "${TRAIN_DIR}" ]; then
+  mkdir -p ${TRAIN_DIR}
+fi
+
 function pwait() {
     while [ $(jobs -p | wc -l) -ge $1 ]; do
         sleep 1
     done
+}
+
+get_seeded_random()
+{
+  seed="$1"
+  openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt \
+    </dev/zero 2>/dev/null
 }
 
