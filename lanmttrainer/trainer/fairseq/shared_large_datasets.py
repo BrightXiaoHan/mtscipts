5cfdaf0ed9a6242390ac976f0a489d5f3089094a
BrightXiaoHan <beatmight@gmail.com>
2022-06-15 16:55:28 +0800

    update code

--- lanmttrainer/trainer/fairseq/shared_large_datasets.py
+++ lanmttrainer/trainer/fairseq/shared_large_datasets.py
@@ -1,8 +1,8 @@
 """Shared Large datasets into parts."""
+import math
 from pathlib import Path
 
 import typer
-from more_itertools import divide
 
 from lanmttrainer.utils import count_lines
 
@@ -18,6 +18,9 @@ def shared_large_datasets(
         "--trainpref",
         help="数据文件前缀。如该参数指定为train，则语言对`en-de`的训练预料路径应为`data-dir`目录下 `train.en-de.en`, `train.en-de.de`。",
     ),
+    suffix_pair: bool = typer.Option(
+            False, "--suffix-pair", help="trainpref与语种标识之间是否加上语言对"
+    )
 ):
     """Sharding very large datasets into parts."""
     # Wrap the data directory in Path object
@@ -27,35 +30,56 @@ def shared_large_datasets(
     lang_pairs = lang_paris.split(",")
 
     # Count the number of lines in each language pair
-    pair2num = {
-        pair: count_lines(data_dir / f"{trainpref}.{pair}.{pair.split('-')[0]}")
-        for pair in lang_pairs
-    }
+    if suffix_pair:
+        pair2num = {
+            pair: count_lines(data_dir / f"{trainpref}.{pair}.{pair.split('-')[0]}")
+            for pair in lang_pairs
+        }
+    else:
+        # TODO 这里判断文件不存在没有进行警告
+        pair2num = {
+            pair: count_lines(data_dir / f"{trainpref}.{pair.split('-')[0]}")
+            for pair in lang_pairs
+        }
 
     typer.echo(f"Num lines in each language pair: {pair2num}")
 
     sum_lines = sum(pair2num.values())
-    num_epoch = sum_lines // epoch_sents + 1
+    num_epoch = math.ceil(sum_lines / epoch_sents)
 
     typer.echo(f"Total chunks: {num_epoch}")
 
-    for pair in lang_pairs:
+    for pair, num in pair2num.items():
+        assert num > 0, f"{pair} has no data"
         src_lang, tgt_lang = pair.split("-")
+        fullprefix = trainpref + "." + pair if suffix_pair else trainpref
+        chunck_size = math.ceil(num / num_epoch)
 
-        srcfile = data_dir / f"{trainpref}.{pair}.{src_lang}"
+        srcfile = data_dir / f"{fullprefix}.{src_lang}"
+        fout = None
+        nthpart = 0
+        for i, line in enumerate(srcfile.open()):
+            if i % chunck_size == 0:
+                if fout is not None:
+                    fout.close()
+                outfile = data_dir / f"part{nthpart}.{fullprefix}.{src_lang}"
+                fout = open(outfile, "w")
+                nthpart += 1
+            fout.write(line)
+        fout.close()
 
-        for i, chunk in enumerate(divide(num_epoch, srcfile.open())):
-            outfile = data_dir / f"part{i}.{trainpref}.{pair}.{src_lang}"
-            with open(outfile, "w") as fout:
-                for line in chunk:
-                    fout.write(line)
-        tgtfile = data_dir / f"{trainpref}.{pair}.{tgt_lang}"
-
-        for i, chunk in enumerate(divide(num_epoch, tgtfile.open())):
-            outfile = data_dir / f"part{i}.{trainpref}.{pair}.{tgt_lang}"
-            with open(outfile, "w") as fout:
-                for line in chunk:
-                    fout.write(line)
+        tgtfile = data_dir / f"{fullprefix}.{tgt_lang}"
+        fout = None
+        nthpart = 0
+        for i, line in enumerate(tgtfile.open()):
+            if i % chunck_size == 0:
+                if fout is not None:
+                    fout.close()
+                outfile = data_dir / f"part{nthpart}.{fullprefix}.{tgt_lang}"
+                fout = open(outfile, "w")
+                nthpart += 1
+            fout.write(line)
+        fout.close()
 
 
 if __name__ == "__main__":
